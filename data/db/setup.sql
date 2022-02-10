25e32da15e48b79230c41fd098a83feb17ba4f5b
hjwilli <hjwilli@gmail.com>
2022-02-10 11:39:58 -0500

    updated synthea output
    generated synthea data for 1000 patients
    updated db data and load script
    generated csv input from "observations" file of synthea data
    
    Closes #14
    Closes #17

--- data/db/setup.sql
+++ data/db/setup.sql
@@ -1,24 +1,42 @@
+CREATE TABLE patients (
+    id                  VARCHAR(36), -- fcfe244e-8fd6-3bad-8d9d-c3fcdd174834
+    birthdate           DATE,
+    deathdate           DATE,
+    ssn                 TEXT,
+    drivers             TEXT,
+    passport            TEXT,
+    prefix              TEXT,
+    first               TEXT,
+    last                TEXT,
+    suffix              TEXT,
+    maiden              TEXT,
+    marital             TEXT,
+    race                TEXT,
+    ethnicity           TEXT,
+    gender              TEXT,
+    birthplace          TEXT,
+    address             TEXT,
+    city                TEXT,
+    state               TEXT,
+    county              TEXT,
+    zip                 INTEGER,
+    lat                 DECIMAL,   -- 39.98525733
+    lon                 DECIMAL,   -- -75.04579348
+    healthcare_expenses MONEY,
+    healthcare_coverage MONEY
+);
+
 CREATE TABLE procedures (
-    procedure_date      DATE,        -- 2013-10-06
+    start              TIMESTAMPTZ,   -- 2011-07-03T07:11:24Z
+    stop               TIMESTAMPTZ,   -- 2011-07-03T07:26:24Z
     patient             VARCHAR(36), -- 684c92a9-7306-4365-9886-8092e7692ea4
     encounter           VARCHAR(36), -- 1d794fe4-389d-4be7-ac05-9b80e5dd3fd9
     code                TEXT,        -- 428191000124101
     description         TEXT,        -- Documentation of current medications
-    cost                MONEY,       -- 586.51
+    base_cost           MONEY,       -- 586.51
     reason_code         TEXT,        -- 10509002, may be null
     reason_description  TEXT         -- Acute bronchitis (disorder), may be null
 );
-
-CREATE TABLE organizations (
-    id                 VARCHAR(36), -- fcfe244e-8fd6-3bad-8d9d-c3fcdd174834
-    name               TEXT,        -- KENSINGTON HOSPITAL
-    address            TEXT,        -- 136 W DIAMOND STREET
-    city               TEXT,        -- PHILADELPHIA
-    state              TEXT,        -- PA
-    zip                TEXT,        -- 19122
-    phone              TEXT,        -- 2154268100
-    utilization        TEXT         -- 17104
-); 
 
 CREATE TABLE providers (
     id                 VARCHAR(36), -- 7817891f-7945-4f3d-b3dc-68df38b908cd
@@ -30,6 +48,8 @@ CREATE TABLE providers (
     city               TEXT,        -- PHILADELPHIA
     state              TEXT,        -- PA
     zip                TEXT,        -- 19122
+    lat                 DECIMAL,   -- 39.98525733
+    lon                 DECIMAL,   -- -75.04579348
     utilization        TEXT         -- 17104
 );
 
@@ -42,11 +62,15 @@ CREATE TABLE encounters (
     -- stop               TIMESTAMP,   -- 2011-07-03T07:26:24Z
 
     patient            VARCHAR(36), -- 684c92a9-7306-4365-9886-8092e7692ea4
+    organization       VARCHAR(36), -- ba8bc7a5-9141-3011-a936-000cd23afd59
     provider           VARCHAR(36), -- fcfe244e-8fd6-3bad-8d9d-c3fcdd174834
+    payer              VARCHAR(36), -- fcfe244e-8fd6-3bad-8d9d-c3fcdd174834
     encounter_class    TEXT,        -- wellness
     code               TEXT,        -- 185349003
     description        TEXT,        -- Encounter for check up (procedure)
-    cost               MONEY,       -- 105.37
+    base_encounter_cost MONEY,       -- 105.37
+    total_claim_cost   MONEY,       -- 105.37
+    payer_coverage     MONEY,       -- 105.37
     reason_code        TEXT,        -- 444814009, may be null
     reason_description TEXT         -- Viral sinusitis (disorder), may be null
 );
@@ -55,10 +79,12 @@ CREATE TABLE medications (
     start              DATE,        -- 2013-09-16
     stop               DATE,        -- 2013-09-23
     patient            VARCHAR(36), -- 8c1709db-6d73-4e04-a8a9-25702eeff152
+    payer              VARCHAR(36), -- b1c428d6-4f07-31e0-90f0-68ffa6ff8c76
     encounter          VARCHAR(36), -- bf2fd769-8e00-47b6-89eb-88354c0bb4f4
     code               TEXT,        -- 562251
     description        TEXT,        -- Amoxicillin 250 MG / Clavulanate 125 MG Oral Tablet
-    cost               MONEY,       -- 23.93
+    base_cost          MONEY,       -- 23.93
+    payer_coverage     MONEY,
     dispenses          SMALLINT,    -- 12
     total_cost         MONEY,       -- 23.93
     reason_code        TEXT,        -- 444814009, may be null
@@ -87,28 +113,29 @@ CREATE TABLE conditions (
 );
 
 
+
+COPY patients(id, birthdate, deathdate, ssn, drivers, passport, prefix, first, last, suffix, maiden, marital, race, ethnicity, gender, birthplace, address, city, state, county, zip, lat, lon, healthcare_expenses, healthcare_coverage) 
+FROM '/db/patients.csv'
+DELIMITER ','
+CSV HEADER;
+
 COPY careplans(id, start, stop, patient, encounter, code, description, reason_code, reason_description) 
 FROM '/db/careplans.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY medications(start, stop, patient, encounter, code, description, cost, dispenses, total_cost, reason_code, reason_description) 
+COPY medications(start, stop, patient, payer, encounter, code, description, base_cost, payer_coverage, dispenses, total_cost, reason_code, reason_description) 
 FROM '/db/medications.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY encounters(id, start, stop, patient, provider, encounter_class, code, description, cost, reason_code, reason_description) 
+COPY encounters(id, start, stop, patient, organization, provider, payer, encounter_class, code, description, base_encounter_cost, total_claim_cost, payer_coverage, reason_code, reason_description) 
 FROM '/db/encounters.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY procedures(procedure_date, patient, encounter, code, description, cost, reason_code, reason_description) 
+COPY procedures(start, stop, patient, encounter, code, description, base_cost, reason_code, reason_description) 
 FROM '/db/procedures.csv'
-DELIMITER ','
-CSV HEADER;
-
-COPY organizations(id, name, address, city, state, zip, phone, utilization) 
-FROM '/db/organizations.csv'
 DELIMITER ','
 CSV HEADER;
 
@@ -117,7 +144,7 @@ FROM '/db/conditions.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY providers(id, organization, name, gender, specialty, address, city, state, zip, utilization) 
+COPY providers(id, organization, name, gender, specialty, address, city, state, zip, lat, lon, utilization) 
 FROM '/db/providers.csv'
 DELIMITER ','
 CSV HEADER;
